3c5efbcf44e7c371be650b4004b7a7ab9ce49879
Davi Costa <davi.costa@ajdesenvolvimento.com.br>
2024-05-22 13:40:56 -0300

    PEDIDO, ESTADOPAGAMENTO e PAGAMENTO

---
+++ src/main/java/com/davicosta/cursomc/domain/PagamentoComBoleto.java
@@ -0,0 +1,47 @@
+package com.davicosta.cursomc.domain;
+
+import java.util.Date;
+
+import com.davicosta.cursomc.domain.enums.EstadoPagamento;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+public class PagamentoComBoleto extends Pagamento {
+	private static final long serialVersionUID = 1L;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dataVencimento;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dataPagamento;
+	
+	public PagamentoComBoleto() {
+	}
+
+	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento, Date dataPagamento) {
+		super(id, estado, pedido);
+		this.dataPagamento = dataPagamento;
+		this.dataVencimento = dataVencimento;
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+	
+	
+}
